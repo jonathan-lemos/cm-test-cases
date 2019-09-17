9d56ea21c81efbd76f0f7a5af3ab5ccd076d9dae
Jonathan Lemos <jonathanlemos@protonmail.com>
2019-09-17 12:53:38 -0400

    added tester program

---
+++ test.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+import os
+import sys
+import re
+import subprocess
+
+if len(sys.argv) < 3:
+    print(f"Usage: {sys.argv[0]} [path of program] [type of program]")
+    sys.exit(0)
+
+if not os.path.exists(sys.argv[1]):
+    print(f"{sys.argv[1]} does not exist")
+    sys.exit(1)
+
+if not os.path.isfile(sys.argv[1]):
+    print(f"{sys.argv[1]} is a directory")
+    sys.exit(1)
+
+if not os.access(sys.argv[1], os.X_OK):
+    print(f"{sys.argv[1]} cannot be executed")
+    sys.exit(1)
+
+base = re.sub("/[^/]*$", "", sys.argv[0]) + "/"
+
+if not os.path.isdir(base):
+    print(f"base dir {base} does not exist. fatal fuckin error")
+    sys.exit(1)
+
+basedir = f"{base}/{sys.argv[2]}"
+if not os.path.isdir(basedir):
+    print(f"no type of program '{sys.argv[2]}'. must be one of {str([x for x in os.listdir(base) if os.path.isdir(x) and not x.startswith('.') and not x.startswith('__')])}")
+    sys.exit(1)
+
+if not os.path.isdir(f"{basedir}/accept"):
+    print(f"accept dir {basedir}/accept does not exist")
+    sys.exit(1)
+
+if not os.path.isdir(f"{basedir}/reject"):
+    print(f"reject dir {basedir}/reject does not exist")
+    sys.exit(1)
+
+failed_accept = []
+failed_reject = []
+len_total = 0
+abspath = os.path.abspath(sys.argv[1])
+for test_case in os.listdir(f"{basedir}/accept"):
+    len_total += 1
+    s = subprocess.check_output([abspath, f"{basedir}/accept/{test_case}"]).decode().strip()
+    if s != "ACCEPT":
+        failed_accept.append((test_case, s.decode().strip()))
+
+for test_case in os.listdir(f"{basedir}/reject"):
+    len_total += 1
+    s = subprocess.check_output([abspath, f"{basedir}/reject/{test_case}"]).decode().strip()
+    if s != "REJECT":
+        failed_reject.append((test_case, s))
+
+for fname, output in failed_accept:
+    print(f"Failed case '{fname}':")
+    print("Expected: ACCEPT")
+    print("Actual: >>>")
+    print(output)
+    print(">>>")
+    print()
+
+for fname, output in failed_reject:
+    print(f"Failed case '{fname}':")
+    print("Expected: REJECT")
+    print("Actual: >>>")
+    print(output)
+    print(">>>")
+    print()
+
+print(f"Failed \033[34m{str(len(failed_accept) + len(failed_reject))}\033[m out of \033[34m{str(len_total)}\033[m cases")
+
